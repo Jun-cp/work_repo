39b22551104b98d820802529a9b417b91f2f1013
Jun-cp <jungjun.lee@kt.com>
2025-02-06 11:03:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -321,11 +321,7 @@ function initDropDownEvents(td) {
       strategySelect.style.display = "inline-block";
       strategySelect.focus();
 
-      // 여러 이벤트를 순차 디스패치
-      ['mousedown','mouseup','click'].forEach(evtType => {
-        const evt = new MouseEvent(evtType, { bubbles: true, cancelable: true, view: window });
-        strategySelect.dispatchEvent(evt);
-      });
+      
     });
   }
 
@@ -342,10 +338,7 @@ function initDropDownEvents(td) {
       detailSelect.style.display = "inline-block";
       detailSelect.focus();
 
-      ['mousedown','mouseup','click'].forEach(evtType => {
-        const evt = new MouseEvent(evtType, { bubbles: true, cancelable: true, view: window });
-        detailSelect.dispatchEvent(evt);
-      });
+      
     });
   }
 
